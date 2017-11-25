0e56542792c386097a2dee8b669fda11bdcfa10f
punam7 <33869422+punam7@users.noreply.github.com>
2017-11-25 11:30:05 +0000

    Update Go.java

--- Go.java
+++ Go.java
@@ -1,1 +1,44 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+//class defnition for reversi game
+public class Go extends Application {
+	// overridden init method
+	public void init() {
+		
+		
+		sp_mainlayout = new StackPane();
+		
+
+	}
+	
+	// overridden start method
+	public void start(Stage primaryStage) {
+		
+		primaryStage.setTitle("Go game");
+		primaryStage.setScene(new Scene(sp_mainlayout,800,800));
+		primaryStage.show();
+
+	}
+	
+	// overridden stop method
+	public void stop() {
+		
+	}
+	
+	// entry point into our program for launching our javafx applicaton
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+	
+	// private fields for a stack pane and a reversi control
+	private StackPane sp_mainlayout;
+	
+}
+
+
+
 
