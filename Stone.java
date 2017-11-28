6689c5c25d8dbacde99b684ca08c05d8e145ff12
x16114426 <charlesbandasteven2@gmail.com>
2017-11-28 00:15:53 +0000

    Update Stone.java

--- Stone.java
+++ Stone.java
@@ -1,48 +1,30 @@
-
 import javafx.scene.Group;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
 import javafx.scene.transform.Translate;
 
-//class definition for a reversi piece
-class GoStone extends Group {
-  
-  	// private fields
-	private int player;		// the player that this stone belongs to
-	private Ellipse piece;	// ellipse representing the player's stone
-	private Translate t;	// translation for the player piece
-  
-	// default constructor for the class
-	public GoStone(int player) {
+// class definition for an X or O piece
+class Stone extends Group {
 
-	}
+// private fields of the class
+private Line l1, l2; // lines for drawing the X piece
+private Ellipse e; // ellipse for rendering the O piece
+private int type; // maintain a copy of the piece type we have
+private Translate pos; // translate that set the position of this piece
+// constructor for the class
 	
-	// overridden version of the resize method to give the stone the correct size
-	@Override
-	public void resize(double width, double height) {
-		super.resize(width, height);
+public Stone(int type) {
+}
+	
+// overridden version of the resize method
+public void resize(double width, double height) {
+// call the super class method
+super.resize(width, height);
+}
+	
+// overridden version of the relocate method
+public void relocate(double x, double y) {
+}
 
-	}
-	
-	// overridden version of the relocate method to position the piece correctly
-	@Override
-	public void relocate(double x, double y) {
-
-	}
-	
-	// public method that will swap the colour and type of this piece
-	public void swapStone() {
-
-	}
-	
-	// method that will set the piece type
-	public void setStone(final int type) {
-		
-	}
-	
-	// returns the type of this piece
-	public int getStone() {
-		// NOTE: this is to keep the compiler happy until you get to this point
-		return 0;
-	}
-	
 }
