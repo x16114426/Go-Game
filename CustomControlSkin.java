1e79b8ffbec7060b61b1755dbf4914e0c33f4b99
x16114426 <charlesbandasteven2@gmail.com>
2017-11-28 00:13:16 +0000

    Update CustomControlSkin.java

--- CustomControlSkin.java
+++ CustomControlSkin.java
@@ -1,1 +1,14 @@
+import javafx.scene.control.Skin;
+import javafx.scene.control.SkinBase;
 
+// class definition for a custom skin
+class CustomControlSkin extends SkinBase<CustomControl> implements
+  
+Skin<CustomControl> {
+  
+public CustomControlSkin(CustomControl cc) {
+  
+// call the super class constructor
+super(cc);
+}
+}
