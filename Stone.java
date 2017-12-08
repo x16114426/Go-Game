224b931e606291f1c3e2c2b2c88938cc8d669435
punam7 <33869422+punam7@users.noreply.github.com>
2017-12-08 20:51:56 +0000

    Update Stone.java

--- Stone.java
+++ Stone.java
@@ -7,33 +7,47 @@ import javafx.scene.transform.Translate;
 // class definition for an X or O piece
 class Stone extends Group {
 
-// private fields of the class
-private Line l1, l2; // lines for drawing the X piece
-private Ellipse stone; // ellipse for rendering the O piece
-private int player; // maintain a copy of the piece type we have
-private Translate pos; // translate that set the position of this piece
 // constructor for the class
 	
-public Stone(int type) {
+public Stone(int player) {
+	pos = new Translate();
+	stone = new Ellipse();
+	//piece.getTransforms().add(t);
+	this.player = player;
+	this.getChildren().add(stone);
+	
+	if (player == 1) {
+		stone.setFill(Color.WHITE);
+	}
+	else if (player == 2){
+		stone.setFill(Color.BLACK);
+	}
+	else if(player==0){
+		stone.setFill(Color.TRANSPARENT);
+	}
 }
 	
 // overridden version of the resize method
 public void resize(double width, double height) {
 // call the super class method
-super.resize(width, height);
+super.resize(width,height);
+stone.setCenterX(width / 2); stone.setCenterY(height / 2);
+stone.setRadiusX(width / 2); stone.setRadiusY(height / 2);
 }
 	
 // overridden version of the relocate method
 public void relocate(double x, double y) {
+	super.relocate(x, y);
+	pos.setX(x); pos.setY(y);
 }
-public int getWBStone() {
+public int getStone() {
 	// NOTE: this is to keep the compiler happy until you get to this point
 	
 	return player;
 }
 
 //method that will set the piece type
-	public void setWBStone(final int type) {
+	public void setStone(final int type) {
 		player=type;
 		if(player == 1 ){
 			stone.setFill(Color.WHITE);
@@ -49,4 +63,10 @@ public int getWBStone() {
 
 		}
 	}
+
+	// private fields of the class
+//	private Line l1, l2; // lines for drawing the X piece
+	private Ellipse stone; // ellipse for rendering the O piece
+	private int player; // maintain a copy of the piece type we have
+	private Translate pos; // translate that set the position of this piece
 }
